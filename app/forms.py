74791dcb2b096dec7fcbc0554366b9190fb962dd
Kamil Legault <legault.kamil@gmail.com>
2020-05-04 00:37:15 -0400

    default val

--- app/forms.py
+++ app/forms.py
@@ -3,6 +3,5 @@ from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
 
 class YoutubeForm(FlaskForm):
-    yt_url = StringField('URLS',validators=[DataRequired()])
+    yt_url = StringField('URLS',default="I0mxnyp2kBw",validators=[DataRequired()])
     submit = SubmitField('Transcribe')
-    print('test')
