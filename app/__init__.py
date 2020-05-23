2271630b83b8989d449c61b5f811d20de3a9f263
Kamil Legault <legault.kamil@gmail.com>
2020-05-22 22:58:22 -0400

    Added flask-bootstrap and removed __pycache__

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask_bootstrap import Bootstrap
 import os
 APP_ROOT = os.path.dirname(os.path.abspath(__file__))
 STATIC_FOLDER = os.path.join(APP_ROOT, 'static')
@@ -7,6 +8,8 @@ STATIC_FOLDER = os.path.join(APP_ROOT, 'static')
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'you-will-never-guess'
 app.config['UPLOAD_FOLDER'] = STATIC_FOLDER
+
+bootstrap = Bootstrap(app)
 
 from app import routes
 
