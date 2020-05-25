06ec237667cd5097777ab6c1cad31e8c2e58dc6d
Kamil Legault <legault.kamil@gmail.com>
2020-05-25 18:26:40 -0400

    removed route

--- app/routes.py
+++ app/routes.py
@@ -14,19 +14,6 @@ def extract_lines(data):
 
 @app.route('/',methods=['GET', 'POST'])
 @app.route('/index',methods=['GET', 'POST'])
-def index():
-    utterances = ["Sentence 1","Sentence 2","Sentence 3"]
-    form = YoutubeForm()
-    default_val = 'I0mxnyp2kBw'
-    trv = YouTubeTranscriptApi.get_transcript(default_val)
-    utterances = extract_lines(trv)
-    if form.validate_on_submit():
-        vidId = form.yt_url.data
-        trv = YouTubeTranscriptApi.get_transcript(vidId)
-        utterances = extract_lines(trv)
-        return render_template('index.html',utterances=utterances,form=form,vidId=vidId)
-    return render_template('index.html',utterances=utterances,form=form,vidId='I0mxnyp2kBw')
-
 @app.route('/video',methods=['GET', 'POST'])
 def video():
     utterances = ["Nothing to diplay yet!", "Just upload a file"]
